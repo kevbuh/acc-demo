f8a595c0af86887d55dceeeea3fbd407a4df5a58
Kevin Buhler <kevinbuhlerr@gmail.com>
2022-01-11 23:32:32 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>ACC</h1>
+      <h2>See where to help.</h2>
+      <p>Use the map below to view where the nearest refugee is</p>
+    </main>
+  );
+}
+
+export default App;
